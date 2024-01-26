572185e9ffbaa0a4214563df0335ad8f8847fee8
IsaacHevi <142832860+IsaacHevi@users.noreply.github.com>
2024-01-26 19:24:56 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from utils import createExcel, findRoots, loadExcel
+
+#Run python main.py to try code on the sample below
+filename = 'quadratic_data.xlsx'
+sheet1 = 'Sheet1'
+sheet2 = 'Sheet2'
+
+createExcel(filename, sheet1, sheet2)
+roots_list = findRoots(filename, sheet1, sheet2)
+loadExcel(filename, sheet1, sheet2, roots_list)
---
+++ utils.py
@@ -0,0 +1,53 @@
+import openpyxl
+import random
+import cmath
+
+def createExcel(filename, sheet1, sheet2):
+    """Creates an excel file with name filename and sheet1 for 100 rows of random numbers and sheet2 for the roots"""
+    wb = openpyxl.Workbook()
+    sheet_1 = wb.create_sheet(title=sheet1)
+    sheet_2 = wb.create_sheet(title=sheet2)
+    
+    for _ in range(100):
+        row_data = [random.randint(1, 10), random.randint(0, 10), random.randint(0, 10)]
+        sheet_1.append(row_data)
+
+    wb.save(filename)
+
+def findRoots(filename, sheet1, sheet2):
+    wb = openpyxl.load_workbook(filename)
+    sheet_1 = wb[sheet1]
+
+    roots_list = []
+
+    for row in sheet_1.iter_rows(min_row=2, values_only=True):
+        a, b, c = row
+        roots = quadratic_roots(a, b, c)
+        roots_list.append(roots)
+
+    wb.close()
+    return roots_list
+
+def loadExcel(filename, sheet1, sheet2, roots_list):
+    """Loads excel sheet2 with the roots"""
+    wb = openpyxl.load_workbook(filename)
+    sheet_2 = wb[sheet2]
+
+    sheet_2.append(['Root 1', 'Root 2'])
+
+    for roots in roots_list:
+        root1, root2 = roots
+
+        root1_str = str(root1)
+        root2_str = str(root2)
+
+        sheet_2.append([root1_str, root2_str])
+
+    wb.save(filename)
+
+def quadratic_roots(a, b, c):
+    """Uses the quadratic formula to find roots"""
+    discriminant = cmath.sqrt(b**2 - 4*a*c)
+    root1 = (-b + discriminant) / (2*a)
+    root2 = (-b - discriminant) / (2*a)
+    return root1, root2
